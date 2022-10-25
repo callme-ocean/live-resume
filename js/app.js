ef8fcf4446d80606936bbd4fb6f9cf8644266898
Sagar <sagarsingh0802@gmail.com>
2022-10-25 19:42:38 +0530

    added counter section

--- js/app.js
+++ js/app.js
@@ -12,6 +12,53 @@ $(document).ready(function() {
         tooltip.innerText = percentage + '%';
         bar.style.width = percentage + '%';
     })
-    console.log(bars);
 
+    // Counter Section - start
+    const counters = document.querySelectorAll('.counter')
+    console.log(counters);
+
+    function runCounter() {
+     counters.forEach(counter => {
+        counter.innerText = 0;
+        let target = +counter.dataset.count;
+        let step = target/100;
+        
+        console.log(target);
+
+        let countIt = function() {
+            let displayedCount = +counter.innerText;
+            if (displayedCount < target) {
+                counter.innerText = Math.ceil(displayedCount + step);
+                setTimeout(countIt, 1);
+            }
+            else {
+                counter.innerText = target;
+            }
+        }
+
+        countIt();
+     })
+
+    }
+
+    runCounter();
+
+    let counterSection = document.querySelector('.counter_section');
+
+    let options = {
+        rootMargin : '0px 0px -200px 0px'
+    }
+
+    let done = 0;
+
+    const sectionObserver = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting && done !== 1) {
+            done = 1;
+            runCounter();
+        }
+    }, options)
+
+    sectionObserver.observe(counterSection);
+
+    // Counter Section - end
 });
