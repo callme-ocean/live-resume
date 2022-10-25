bbc6b86d5ccfd425cace715faa51fa3b8e462299
Sagar <sagarsingh0802@gmail.com>
2022-10-25 18:44:15 +0530

    added Education and Skills section

--- js/app.js
+++ js/app.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
         dropRadius: 10
     });
 
+    const bars = document.querySelectorAll('.progress_bar');
+    bars.forEach(function(bar) {
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+    })
+    console.log(bars);
+
 });
